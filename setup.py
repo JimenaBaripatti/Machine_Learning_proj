9ce3031b723fca1467dbb4f64bf047997f6b3892
Jimena Baripatti <jimenabaripatti@gmail.com>
2019-04-15 21:21:16 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=': A complete Machine Learning Project. I will compare the performance of a Random Forest and a Neural Networks classifiers.',
+    author='JimenaBaripatti',
+    license='MIT',
+)
